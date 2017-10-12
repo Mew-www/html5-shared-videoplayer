af0bccad4be1a55462ab916035dd28146daf071c
Mew-www <mew.proxy@hotmail.com>
2017-10-12 17:29:48 +0300

    Initial working commit (update videolist and videoplayer)

---
+++ sources/js/settings.js
@@ -0,0 +1,3 @@
+export default {
+  VIDEO_DIRECTORY_URI: 'http://62.78.180.5/streamprojekti/'
+};
--- sources/js/subcomponents/videolist.jsx
+++ sources/js/subcomponents/videolist.jsx
@@ -1,16 +1,39 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import {Video} from "../models/video";
+import Settings from "../settings";
 
 export class VideolistComponent extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      available_videos: []
+      available_videos: null
     };
-    axios.get('http://62.78.180.5/streamprojekti/')
+    axios.get(Settings.VIDEO_DIRECTORY_URI)
       .then((response) => {
-        console.log(response.data);
+        let tmpDOM = document.createElement('div');
+        tmpDOM.innerHTML = response.data;
+        let content_rows = Array.from(tmpDOM.querySelectorAll("tr")).slice(3,-1);
+        let videos = content_rows.map((row_element) => {
+          //the-disaster-artist-trailer-2_h1080p.mov
+          let filename = row_element.querySelector("td:nth-child(2) > a").getAttribute('href');
+          let modtime = row_element.querySelector("td:nth-child(3)").innerHTML;
+          let filesize = row_element.querySelector("td:nth-child(4)").innerHTML;
+
+          let title = filename
+              .split('_').slice(0,-1).join(' ')                                               // Clip title
+              .split('-').join(' ')                                                           // Replace -'s with whitespace
+              .split('').map((char, idx) => (idx === 0 ? char.toUpperCase() : char)).join('') // Capitalize
+            + ` ${filename.indexOf('h1080') > -1 ? 'HD' : 'LOWRES'}`                          // Append resolution
+            + ` (${filesize.trim()})`;                                                        // Append filesize
+          return new Video(
+            title,
+            Settings.VIDEO_DIRECTORY_URI + filename,
+            ''
+          );
+        });
+        this.setState({available_videos: videos})
       })
       .catch((error) => {
         console.log(error);
@@ -21,7 +44,16 @@ export class VideolistComponent extends Component {
     return (
       <div className="Videolist">
         {/* has this.props.changeVideo(video_class_instance) */}
-        <button>todo generate these</button>
+        {this.state.available_videos === null ?
+          <p>Loading</p>
+          :
+          !this.state.available_videos ?
+            <p>No videos found</p>
+            :
+            this.state.available_videos.map(
+              (video) => <button key={video.source} onClick={()=>this.props.changeVideo(video)}>{video.name}</button>
+            )
+        }
       </div>
     );
   }
